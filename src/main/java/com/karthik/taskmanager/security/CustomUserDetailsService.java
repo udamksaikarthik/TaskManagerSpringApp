69509f8d5ba73b5ec7a8546b3dd1aa4dae6b9436
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2025-09-17 10:04:00 +0100

    Minor Change in imports

--- src/main/java/com/karthik/taskmanager/security/CustomUserDetailsService.java
+++ src/main/java/com/karthik/taskmanager/security/CustomUserDetailsService.java
@@ -1,8 +1,6 @@
 package com.karthik.taskmanager.security;
 
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
